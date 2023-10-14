9a087c0cc925d50ef86534128105d79c646d5317
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-10-14 18:50:03 -0500

    update

--- server/controller/route_controller.js
+++ server/controller/route_controller.js
@@ -1,56 +1,55 @@
-const axios = require('axios');
-const apikey = process.env.API_KEY
+const axios = require("axios");
+const apiKey = process.env.API_KEY;
+require("dotenv").config();
 
-
-
-exports.hero_page = async(req, res) =>{
-    try {
-        const hero_data = await axios.get(
-          ` https://newsdata.io/api/1/news?apikey=${apikey}&language=en&image=1`
-        );
-        res.send(hero_data.data);
-      } catch (error) {
-        console.log(error);
-      }
-}
-exports.business = async(req, res) =>{
-   try {
+exports.hero_page = async (req, res) => {
+  try {
+    const hero_data = await axios.get(
+      ` https://newsdata.io/api/1/news?apikey=${apiKey}&language=en&image=1`
+    );
+    res.send(hero_data.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+exports.business = async (req, res) => {
+  try {
     const business_data = await axios.get(
-      `https://newsdata.io/api/1/news?apikey=${apikey}&category=business&language=en&image=1`
+      `https://newsdata.io/api/1/news?apikey=${apiKey}&category=business&language=en&image=1`
     );
     res.send(business_data.data);
   } catch (error) {
     console.log(error);
   }
-}
-exports.science = async(req, res) =>{
-    try {
-        const science_data = await axios.get(
-          `https://newsdata.io/api/1/news?apikey=${apikey}&category=science&language=en&image=1`
-        );
-        res.send(science_data.data);
-      } catch (error) {
-        console.log(error);
-      }
-}
-exports.sports = async(req, res) =>{
-    try {
-        const sports_data = await axios.get(
-          `https://newsdata.io/api/1/news?apikey=${apikey}&category=sports&language=en&image=1`
-        );
-        res.send(sports_data.data);
-      } catch (error) {
-        console.log(error);
-      }
-}
-exports.search = async(req, res) =>{
+};
+exports.science = async (req, res) => {
+  try {
+    const science_data = await axios.get(
+      `https://newsdata.io/api/1/news?apikey=${apiKey}&category=science&language=en&image=1`
+    );
+    res.send(science_data.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+exports.sports = async (req, res) => {
+  try {
+    const sports_data = await axios.get(
+      `https://newsdata.io/api/1/news?apikey=${apiKey}&category=sports&language=en&image=1`
+    );
+    res.send(sports_data.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+exports.search = async (req, res) => {
   const { search } = req.body;
-    try {
-        const search_data = await axios.get(
-          `https://newsdata.io/api/1/news?apikey=${apikey}&qInTitle=${search}&language=en&image=1`
-        );
-        res.send(search_data.data);
-      } catch (error) {
-        console.log(error);
-      }
-}
+  try {
+    const search_data = await axios.get(
+      `https://newsdata.io/api/1/news?apikey=${apiKey}&qInTitle=${search}&language=en&image=1`
+    );
+    res.send(search_data.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
