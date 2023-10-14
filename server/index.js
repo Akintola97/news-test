182e2b7ec67789d4e3202df35c4738c3db7ab2e0
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-10-14 18:14:11 -0500

    update

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const route = require('./routes/routes')
 
 
 
-app.use(cors({ credentials: true, origin: "https://news-search-app.onrender.com"}));
+app.use(cors({ credentials: true, origin: "https://news-app-2l4t.onrender.com"}));
 app.use(express.json());
 
 
